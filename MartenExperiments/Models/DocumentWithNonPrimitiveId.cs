ec82c6d4c4e988049043bae84f8c013a74898cc8
Damian Reeves <DamReev@gmail.com>
2017-02-22 12:20:17 -0500

    Confirmed we need to stick with primitive obsession

---
+++ MartenExperiments/Models/DocumentWithNonPrimitiveId.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Baseline;
+using Marten.Schema;
+using Marten.Schema.Identity;
+using StringlyTyped;
+
+namespace MartenExperiments.Models
+{
+    public class DocumentWithNonPrimitiveId
+    {
+        public DocumentId Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class DocumentId : Stringly<Guid>
+    {
+        public DocumentId(string value) : base(value)
+        {
+        }
+    }
+
+    public class DocumentIdGenerationStrategy : IIdGeneration, IIdGenerator<DocumentId>
+    {
+        public IEnumerable<Type> KeyTypes { get; } = new[]
+        {
+            typeof(DocumentId)
+        };
+
+        public IIdGenerator<T> Build<T>(IDocumentSchema schema)
+        {
+            return this.As<IIdGenerator<T>>();
+        }
+
+        public DocumentId Assign(DocumentId existing, out bool assigned)
+        {
+            if (existing == null || existing.Result == Guid.Empty || String.IsNullOrWhiteSpace(existing.Value))
+            {
+                assigned = true;
+                return new DocumentId(Guid.NewGuid().ToString());
+            }
+
+            assigned = false;
+            return existing;
+
+        }
+    }
+}
---
+++ MartenExperiments/Primitive_obsession_buster.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Marten;
+using Marten.Schema.Identity;
+using MartenExperiments.Models;
+using MartenExperiments.Testing;
+using Ploeh.AutoFixture.Xunit2;
+using StringlyTyped;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MartenExperiments
+{
+    public class Primitive_obsession_buster : MartenTestBase
+    {
+        public Primitive_obsession_buster(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        
+        [Theory(Skip = "Not Supported")]
+        [AutoData(Skip = "Not Supported")]
+        public async Task Should_be_able_to_save_document_with_non_primitive_Id(Guid documentId, string documentName)
+        {
+            var id = new DocumentId(documentId.ToString());
+            using (var store = CreateDocumentStore(_ =>
+            {
+                _.AutoCreateSchemaObjects = AutoCreate.All;
+                _.Schema
+                    .For<DocumentWithNonPrimitiveId>()
+                    .IdStrategy(new DocumentIdGenerationStrategy());
+            }))
+            {                
+                using (var session = store.OpenSession())
+                {
+                    var doc = new DocumentWithNonPrimitiveId
+                    {
+                        Id = id,
+                        Name = documentName
+                    };
+
+                    var existingDoc = await session.LoadAsync<DocumentWithNonPrimitiveId>(doc.Id.ToString());
+                    if (existingDoc != null)
+                    {
+                        Output.WriteLine($"Document: {existingDoc.Id} already exists!");
+                    }
+
+                    session.Store(doc);
+                    await session.SaveChangesAsync();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var retrievedDoc = await session.LoadAsync<DocumentWithNonPrimitiveId>(id.ToString());
+                    retrievedDoc.ShouldBeEquivalentTo(new
+                    {
+                        Id = id,
+                        Name = documentName
+                    });
+                }
+                
+            }
+        }
+    }
+}
